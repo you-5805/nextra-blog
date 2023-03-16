68b022b6f7e0618b8badd299274c9a40c196204d
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-03-16 19:40:58 +0900

    update footer

--- theme.config.jsx
+++ theme.config.jsx
@@ -7,4 +7,29 @@ export default {
   feedback: {
     content: null,
   },
+  footer: {
+    component: () => {
+      return (
+        <footer
+          style={{
+            padding: 20,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 28,
+          }}
+        >
+          <a href='https://yoiw.dev' target='_blank' rel='noreferrer noopener'>
+            About me
+          </a>
+          <a href='https://twitter.com/yoiwamoto' target='_blank' rel='noreferrer noopener'>
+            Twitter
+          </a>
+          <a href='https://github.com/you-5805' target='_blank' rel='noreferrer noopener'>
+            GitHub
+          </a>
+        </footer>
+      );
+    },
+  },
 };
