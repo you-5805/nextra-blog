295fa4e178ab527568e1d32852fc02e6f6a4f19c
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-03-16 18:51:11 +0900

    fix: sidebar layout, update entry

--- src/pages/api/test/index.tsx
+++ src/pages/api/test/index.tsx
@@ -6,8 +6,15 @@
 
 // export default handler;
 
+import { NextApiHandler } from 'next';
 import { type NextMiddleware, NextResponse } from 'next/server';
 
-export const GET = ((req) => {
-  return NextResponse.json({ msg: 'Hello world!' });
-}) satisfies NextMiddleware;
+export const GET = (req: Request) => {
+  req.headers
+  return new Response('Hello world!');
+}
+
+const handler = ((req, res) => {
+  res.json({ msg: 'Hello world!' });
+  res.
+}) satisfies NextApiHandler;
